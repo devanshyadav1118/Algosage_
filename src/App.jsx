63ac7541771bdd7df011ed0e5348c6c9a7f55dd0
devanshyadav1118 <103892968+devanshyadav1118@users.noreply.github.com>
2024-02-05 16:19:57 +0530

    Made and attached the About page as well as Contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
+import About from "./Pages/About/About";
 import Homepage from "./Pages/Homepage";
-import {BrowserRouter as Router, Routes,Route } from "react-router-dom";
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Contact from "./Pages/Contact/Contact";
 
 const App = () => {
   return (
@@ -11,8 +11,10 @@ const App = () => {
       <Router>
         <div>
           <Routes>
-            <Route exact path="/" element={<Homepage/>} />
-       
+            <Route exact path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
       </Router>
---
+++ src/Pages/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../../Components/Navbar";
+import Footer from "../../Components/Footer";
+import Contactform from "../../Components/Contactform";
+import './Contact.css'
+const Contact = () => {
+  return (
+    <>
+      <Navbar/>
+      <Contactform />
+      <Footer />
+    </>
+  );
+};
+
+export default Contact;
