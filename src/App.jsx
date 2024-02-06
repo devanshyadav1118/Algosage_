af2e234896911a4b6b97e356de7bde60b483ac19
devanshyadav1118 <103892968+devanshyadav1118@users.noreply.github.com>
2024-02-06 14:31:52 +0530

    Added and designed Courses Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./Pages/About/About";
 import Homepage from "./Pages/Homepage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from "./Pages/Contact/Contact";
+import Courses from "./Pages/Courses/Courses.jsx";
 
 const App = () => {
   return (
@@ -13,7 +14,7 @@ const App = () => {
           <Routes>
             <Route exact path="/" element={<Homepage />} />
             <Route path="/about" element={<About />} />
-
+            <Route path="/courses" element={<Courses/>} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
---
+++ src/Pages/Courses/Courses.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../../Components/Navbar";
+import Footer from "../../Components/Footer";
+import Section from "../../Components/Section";
+
+const Courses = () => {
+  return (
+    <div>
+      <Navbar />
+      <Section />
+      <Footer /> 
+    </div>
+  );
+};
+
+export default Courses;
